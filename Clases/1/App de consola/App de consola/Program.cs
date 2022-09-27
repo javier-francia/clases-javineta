93c7b151b5c9b0f105f45722eb6b7d3c1dd8f498
Javier Francia <javifrancia@gmail.com>
2022-09-26 21:25:27 -0300

    documentos

---
+++ Clases/1/App de consola/App de consola/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace App_de_consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese un numero del 1 al 10: ");
+            Console.WriteLine("");
+
+            string ingresado = Console.ReadLine();
+
+            int numero = Convert.ToInt32(ingresado);
+
+
+            //int numero = parseInt(ingresado);
+            //var mensaje = "Ingresaste un " + numero;
+
+            Console.WriteLine(numero * 2);
+        }
+    }
+}
